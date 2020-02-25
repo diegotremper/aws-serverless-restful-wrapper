56bb69db8908e0f116de6613f0aa4d3c91fda2ef
Tremper, Diego (ESI) <diego.tremper@adp.com>
2020-02-24 23:21:36 -0300

    fix(request):Fix HTTP method typo

--- src/lib/resource.ts
+++ src/lib/resource.ts
@@ -2,5 +2,5 @@ import { eventHandlerFactory } from './event-factory';
 import { WrapperConfig } from './config';
 
 export const resource = (config: WrapperConfig): Function => {
-  return eventHandlerFactory(config, ['GET', 'POST', 'PUT', 'PATCH', 'DElETE']);
+  return eventHandlerFactory(config, ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']);
 };
