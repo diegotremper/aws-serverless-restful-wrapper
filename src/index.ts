355248e4e8e0f88e010474ec5e166bc0eded7e96
Diego Tremper <diegotremper@gmail.com>
2020-02-28 00:01:30 -0300

    fix(): Library DX

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import { collection } from './lib/collection';
 import { document } from './lib/document';
-import { logger } from './lib/logger';
-import { vendors } from './lib/vendors';
+import * as l from './lib/logger';
+import * as v from './lib/vendors';
 
-export default {
-  api: {
-    collection,
-    document
-  },
-  logger,
-  vendors
+export const api = {
+  collection,
+  document
 };
+
+export const logger = l.logger;
+export const vendors = v.vendors;
